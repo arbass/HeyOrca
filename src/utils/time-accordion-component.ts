f233f46859448272f31d50f551683caf0df72ebf
arbass <13ar.bass@gmail.com>
2023-08-17 00:49:31 +0300

    added: animateRow() , findActiveTab()

--- src/utils/time-accordion-component.ts
+++ src/utils/time-accordion-component.ts
@@ -5,37 +5,90 @@ export const timeAccordionComponent = () => {
   if (tabComponent) {
     fsAttributes.cmsnest.loading
       .then(() => {
+        let rowInterval;
         const allRows = document.querySelectorAll('.cl-i_time-accordion_widget-content-row');
+        allRows[0].classList.add('active');
         allRows.forEach((row) => {
           row.addEventListener('click', () => {
-            const currentProgressBar = row.querySelector(
-              '.time-accordion_widget-content-row-progress'
-            );
-            const currentArrowIcon = row.querySelector('.arrow-icon');
-            const currentContentP = row.querySelector('.is-time-accordion_widget-content-row-col');
-            console.log(currentContentP);
-
-            const allProgressBar = document.querySelectorAll(
-              '.time-accordion_widget-content-row-progress'
-            );
-            const allArrowIcon = document.querySelectorAll('.arrow-icon');
-            const allContentP = document.querySelectorAll(
-              '.is-time-accordion_widget-content-row-col'
-            );
-            allProgressBar.forEach((progressBar) => {
-              progressBar.classList.remove('animated');
-            });
-            allArrowIcon.forEach((arrowIcon) => {
-              arrowIcon.classList.remove('animated');
-            });
-            allContentP.forEach((contentP) => {
-              contentP.classList.remove('animated');
-            });
-            currentProgressBar.classList.add('animated');
-            currentArrowIcon.classList.add('animated');
-            currentContentP.classList.add('animated');
+            animateRow(row);
           });
         });
+
+        function animateRow(el) {
+          const currentProgressBar = el.querySelector(
+            '.time-accordion_widget-content-row-progress'
+          );
+          const currentArrowIcon = el.querySelector('.arrow-icon');
+          const currentContentP = el.querySelector('.is-time-accordion_widget-content-row-col');
+
+          const allProgressBar = document.querySelectorAll(
+            '.time-accordion_widget-content-row-progress'
+          );
+          const allArrowIcon = document.querySelectorAll('.arrow-icon');
+          const allContentP = document.querySelectorAll(
+            '.is-time-accordion_widget-content-row-col'
+          );
+          allProgressBar.forEach((progressBar) => {
+            progressBar.classList.remove('animated');
+          });
+          allArrowIcon.forEach((arrowIcon) => {
+            arrowIcon.classList.remove('animated');
+          });
+          allContentP.forEach((contentP) => {
+            contentP.classList.remove('animated');
+          });
+          currentProgressBar.classList.add('animated');
+          currentArrowIcon.classList.add('animated');
+          currentContentP.classList.add('animated');
+        }
+
+        function rowsOrderFromActive() {
+          const activeRow = document.querySelector(
+            '.cl-i_time-accordion_widget-content-row.active'
+          );
+          animateRow(activeRow);
+          rowInterval = setInterval(() => {}, 3500);
+        }
+
+        function findActiveTab() {
+          const activeTab = document.querySelector('.time-accordion_inner .w--tab-active');
+          allRows.forEach((row) => {
+            row.classList.remove('active');
+          });
+          const tabsRows = activeTab.querySelectorAll('.cl-i_time-accordion_widget-content-row');
+          tabsRows[0].classList.add('active');
+          rowsOrderFromActive();
+        }
+
+        let lastExecutionTime = 0;
+
+        function throttle(func, delay) {
+          return function (...args) {
+            const currentTime = Date.now();
+            if (currentTime - lastExecutionTime >= delay) {
+              func.apply(this, args);
+              lastExecutionTime = currentTime;
+            }
+          };
+        }
+
+        function handleClassChanges(mutationsList, observer) {
+          for (const mutation of mutationsList) {
+            if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
+              throttledFindActiveTab();
+            }
+          }
+        }
+
+        const tabPanes = document.querySelectorAll('.w-tab-pane');
+        const observer = new MutationObserver(handleClassChanges);
+        const config = { attributes: true, attributeFilter: ['class'] };
+
+        tabPanes.forEach((tabPane) => {
+          observer.observe(tabPane, config);
+        });
+
+        const throttledFindActiveTab = throttle(findActiveTab, 100);
       })
       .catch((error) => {
         console.error('Error:', error);
