7bc2726cb1a7d2353cad20a4cb9b35a0f92ddb90
arbass <13ar.bass@gmail.com>
2023-08-17 11:45:40 +0300

    added: comments

--- src/utils/time-accordion-component.ts
+++ src/utils/time-accordion-component.ts
@@ -1,31 +1,49 @@
 /* eslint-disable no-console */
 
+// This function represents a time accordion component.
 export const timeAccordionComponent = () => {
+  // Get the HTML element with the attribute [time-accordion-component].
   const tabComponent = document.querySelector('[time-accordion-component]');
+
   if (tabComponent) {
+    // Load some attributes from a Content Management System.
     fsAttributes.cmsnest.loading
       .then(() => {
+        //empty var of the interval
         let rowInterval;
-        const allRows = document.querySelectorAll('.cl-i_time-accordion_widget-content-row');
+
+        // Get all rows of the time accordion.
+        const allRows = document.querySelectorAll(
+          '.time-accordion_inner .cl-i_time-accordion_widget-content-row'
+        );
+
+        // Set the first row as active.
         allRows[0].classList.add('active');
+
+        // Add click event listeners to each row.
         allRows.forEach((row) => {
           row.addEventListener('click', () => {
             clearInterval(rowInterval);
+            // Remove active class from all rows.
             allRows.forEach((row) => {
               row.classList.remove('active');
             });
+            // Add active class to the clicked row.
             row.classList.add('active');
             rowsOrderFromActive();
           });
         });
 
+        // Function to animate a row's elements.
         function animateRow(el) {
+          // Get elements within the row.
           const currentProgressBar = el.querySelector(
             '.time-accordion_widget-content-row-progress'
           );
           const currentArrowIcon = el.querySelector('.arrow-icon');
           const currentContentP = el.querySelector('.is-time-accordion_widget-content-row-col');
 
+          // Get similar elements from all rows.
           const allProgressBar = document.querySelectorAll(
             '.time-accordion_widget-content-row-progress'
           );
@@ -33,6 +51,8 @@ export const timeAccordionComponent = () => {
           const allContentP = document.querySelectorAll(
             '.is-time-accordion_widget-content-row-col'
           );
+
+          // Remove 'animated' class from all similar elements.
           allProgressBar.forEach((progressBar) => {
             progressBar.classList.remove('animated');
           });
@@ -42,37 +62,47 @@ export const timeAccordionComponent = () => {
           allContentP.forEach((contentP) => {
             contentP.classList.remove('animated');
           });
+
+          // Add 'animated' class to the current row's elements.
           currentProgressBar.classList.add('animated');
           currentArrowIcon.classList.add('animated');
           currentContentP.classList.add('animated');
         }
 
+        // Function to handle the order of rows when active.
         function rowsOrderFromActive() {
           clearInterval(rowInterval);
+          // Get the currently active row.
           let activeRow = document.querySelector('.cl-i_time-accordion_widget-content-row.active');
           animateRow(activeRow);
           rowInterval = setInterval(() => {
             if (activeRow.nextElementSibling) {
+              // Move to the next row if available.
               activeRow.classList.remove('active');
               activeRow = activeRow.nextElementSibling;
               activeRow.classList.add('active');
               animateRow(activeRow);
             } else {
+              // Start from the first row if the end is reached.
               const firstRow = activeRow.parentNode.firstChild;
               activeRow.classList.remove('active');
               activeRow = firstRow;
               activeRow.classList.add('active');
               rowsOrderFromActive();
             }
-          }, 3500);
+          }, 3500); // Interval duration in milliseconds.
         }
 
+        // Function to find the active tab and set its rows as active.
         function findActiveTab() {
           clearInterval(rowInterval);
           allRows.forEach((row) => {
             row.classList.remove('active');
           });
+
+          // Find the active tab element.
           const activeTab = document.querySelector('.time-accordion_inner .w--tab-active');
+          // Get rows within the active tab.
           const tabsRows = activeTab.querySelectorAll('.cl-i_time-accordion_widget-content-row');
           tabsRows[0].classList.add('active');
           rowsOrderFromActive();
@@ -80,6 +110,7 @@ export const timeAccordionComponent = () => {
 
         let lastExecutionTime = 0;
 
+        // Throttle function to limit execution frequency.
         function throttle(func, delay) {
           return function (...args) {
             const currentTime = Date.now();
@@ -90,6 +121,7 @@ export const timeAccordionComponent = () => {
           };
         }
 
+        // Function to handle class changes using a MutationObserver.
         function handleClassChanges(mutationsList, observer) {
           for (const mutation of mutationsList) {
             if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
@@ -98,15 +130,20 @@ export const timeAccordionComponent = () => {
           }
         }
 
+        // Get all tab pane elements.
         const tabPanes = document.querySelectorAll('.w-tab-pane');
         const observer = new MutationObserver(handleClassChanges);
         const config = { attributes: true, attributeFilter: ['class'] };
 
+        // Observe class changes in tab panes.
         tabPanes.forEach((tabPane) => {
           observer.observe(tabPane, config);
         });
 
+        // Apply throttle to the findActiveTab function.
         const throttledFindActiveTab = throttle(findActiveTab, 200);
+
+        // Call findActiveTab after a delay.
         setTimeout(() => {
           findActiveTab();
         }, 500);
