ee45ed672c2b11007d47f155585e7a5e94b69c16
Chad Roberts <chad@heyorca.com>
2024-08-16 09:34:05 -0230

    Update script v7.

--- src/utils/sign-up-form.ts
+++ src/utils/sign-up-form.ts
@@ -10,7 +10,6 @@ export const signUpForm = () => {
 
         const formData = new FormData(signUpForm);
         const reqData = Object.fromEntries(formData.entries());
-        reqData.ho_medium = 'cpc';
         reqData.pageId = 'some-uuid';
 
         try {
@@ -26,47 +25,13 @@ export const signUpForm = () => {
           const result = await response.json();
 
           if (result.success && result.redirect && result.redirectPath) {
-            // Handle sending data to HubSpot
-            await fetch(
-              `https://api.hsforms.com/submissions/v3/integration/submit/596637/497517de-b0f2-4351-b3ba-cf9109c4611a`,
-              {
-                keepalive: true,
-                method: 'POST',
-                headers: {
-                  'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                  submittedAt: Date.now(),
-                  context: {
-                    pageUri: 'heyorca.com',
-                    pageName: document.title,
-                  },
-                  fields: [
-                    {
-                      objectTypeId: '0-1',
-                      name: 'email',
-                      value: reqData.email,
-                    },
-                    {
-                      objectTypeId: '0-1',
-                      name: 'industry_type',
-                      value: reqData.industryType,
-                    },
-                    {
-                      objectTypeId: '0-1',
-                      name: 'self_reported_attribution',
-                      value: reqData.source,
-                    },
-                  ],
-                }),
-              }
-            );
-
             // Handle navigating to app
             window.location.assign(result.redirectPath);
+            return false;
           }
         } catch (error) {
           console.log(error);
+          return false;
         }
 
         return false;
