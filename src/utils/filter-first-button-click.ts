f4dc7b4507b1d18ebc97d42774780258bbf22a58
Chad Roberts <chad@heyorca.com>
2026-09-03 16:42:09 -0230

    Stop first click for blogs if query param exists

--- src/utils/filter-first-button-click.ts
+++ src/utils/filter-first-button-click.ts
@@ -3,11 +3,15 @@
 export const filterFirstButtonClick = () => {
   const filterFirstButtonClick_element = document.querySelector('.form-filter');
   if (filterFirstButtonClick_element) {
+    const isBlogPage = !!document.getElementById('blog-page');
+    const categoryParam = new URLSearchParams(window.location.search).get('category');
+    if (isBlogPage && categoryParam) return;
+
     setTimeout(function () {
-      const firstFilterButton = filterFirstButtonClick_element.querySelector(
+      const firstFilterButton = filterFirstButtonClick_element.querySelector<HTMLElement>(
         '.time-accordion_nav .button'
       );
-      firstFilterButton.click();
+      firstFilterButton?.click();
     }, 2000);
   }
 };
